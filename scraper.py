e5e176a7f9cdb9cb3ecda4df7ff292abefb7e8da
Azan Moha <white.ghoost9968@gmail.com>
2025-12-07 15:16:32 +0400

    Saving Scraped Data to csv file

--- scraper.py
+++ scraper.py
@@ -1,27 +1,31 @@
+from hmac import new
 from bs4 import BeautifulSoup
 import requests
 import urllib3 
-
+import csv
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
 
 url = "https://scrapeme.live/shop/"
-response = requests.get(url)
+response = requests.get(url, verify=False)
 soup = BeautifulSoup(response.text, 'html.parser')
 
 
 ## Getting Products Names 
 product_name = soup.find_all( "h2", class_ = "woocommerce-loop-product__title")
 
-for product in product_name:
-    print(product.text)
-
 ## Getting Products Prices
 product_price= soup.find_all("span", class_="woocommerce-Price-amount amount")
-for price in product_price:
-    print(price.text)
 
-for product, price in zip(product_name, product_price):
-    print(f"{product.text} - {price.text}")
+## Saving Data to csv file
+with open('scraped_data.csv', 'w', newline='', encoding='utf-8') as file : 
+    writer= csv.writer(file)
+    writer.writerow(['Product Name','Price'])
 
- 
+    for product, price in zip (product_name, product_price):
+        name_text= product.text
+        price_text= price.text 
+        writer.writerow([name_text, price_text])
+
+
+        print(f"Saved: {name_text} - {price_text}")
